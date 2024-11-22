701ff1ed6bfe017cdcc34f5ff8c2d78e8ef5ebde
Kaizhi Tang <kaizhi.tang@anywhere.re>
2024-11-22 16:38:24 -0500

    update the git_redeploy.sh with the new configurations.

--- docker/git_redeploy.sh
+++ docker/git_redeploy.sh
@@ -6,13 +6,19 @@ git pull
 echo "Turn down the docker containers ..."
 sudo docker compose down
 
-echo "Remove knowledge library ragflow image ..."
-sudo docker rmi infiniflow/ragflow1:dev
+#echo "Remove knowledge library ragflow image ..."
+sudo docker rmi infiniflow/ragflow:dev-slim
 
 echo "Building the knowledge library ragflow image..."
-sudo docker compose -f ragkl-docker-compose.yml build ragflow
+#sudo docker compose -f ragkl-docker-compose.yml build ragflow
+cd ..
+source ./venv/bin/activate
+sudo docker build -f Dockerfile.slim -t infiniflow/ragflow:dev-slim .
+deactivate
+cd docker
 
 echo "Turn on the docker containers ..."
-sudo docker compose -f ragkl-docker-compose.yml up -d
+#sudo docker compose -f ragkl-docker-compose.yml up -d
+sudo docker compose -f docker-compose.yml up -d
 
 echo "Containers started successfully."
